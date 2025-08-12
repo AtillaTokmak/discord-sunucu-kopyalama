df8871a4bd47e100715d626ead5d30f0ae14334f
Atilla Tokmak <161002147+AtillaTokmak@users.noreply.github.com>
2025-08-12 21:45:51 +0300

    Update guild_manager.py

--- cogs/guild_manager.py
+++ cogs/guild_manager.py
@@ -1,187 +1,174 @@
+import discord
 from discord.ext import commands
 import asyncio
 import json
 import os
-import discord
-from datetime import datetime
 
-
-async def remove_channels(ctx: commands.Context):
-    tasks = []
-    for channel in ctx.guild.channels:
-        tasks.append(asyncio.create_task(channel.delete()))
-    await asyncio.gather(*tasks)
-
-
-async def remove_roles(ctx: commands.Context):
-    tasks = []
-    for role in ctx.guild.roles:
-        if role == ctx.guild.default_role:
-            continue
-        elif role.managed:
-            continue
-        tasks.append(asyncio.create_task(role.delete()))
-    await asyncio.gather(*tasks)
-
-async def gather_overwrites(element : discord.channel):
-    overwrites = {}
-    thing = element.overwrites
-    for overwrite in thing:
-        if overwrite.managed:
-            continue
-        role_overwrites = dict(thing[overwrite])
-        overwrites[overwrite.name] = role_overwrites
-        
-    return overwrites
-
-async def get_channels(ctx: commands.Context):
-    channels = {}
-    for channel in ctx.guild.channels:
-        if channel.type == discord.ChannelType.category:
-            continue
-        channel_overwrites = await gather_overwrites(channel)
-
-        if channel.last_message_id:
-            last_message = [message async for message in channel.history(limit=1)][0]
-            last_message = last_message.content
-        else:
-            last_message = None
-        channels[channel.name] = {
-            "last_message": last_message,
-            "type": channel.type.name,
-            "position": channel.position,
-            "category": channel.category.name if channel.category else None,
-            "nsfw": channel.nsfw,
-            "permissions_synced": channel.permissions_synced,
-            "overwrites": None if channel.permissions_synced else channel_overwrites,
-        }
-
-    return channels
-
-async def get_categories(ctx: commands.Context):
-    categories = {}
-    
-    for category in ctx.guild.categories:
-        channel_overwrites = await gather_overwrites(category)
-
-        categories[category.name] = {
-            "position": category.position,
-            "overwrites": channel_overwrites
-            }
-
-    return categories
-
-async def get_roles(ctx: commands.Context):
-    roles = {}
-    for role in ctx.guild.roles:
-        if role.managed: #if role is a bot role
-            continue
-        roles[role.name] = {
-            "position": role.position,
-            "colour": {
-                "r": role.colour.r,
-                "g": role.colour.g,
-                "b": role.colour.b
-            },
-            "mentionable": role.mentionable,
-            "permissions": dict(role.permissions)
-        }
-    return roles
-
-async def create_roles(ctx: commands.Context, guild):
-    roles = {}
-    tasks = []
-    for role in guild['roles']:
-        params = {
-            'name': role,
-            "mentionable": guild['roles'][role]['mentionable'],
-            "colour": discord.Colour.from_rgb(**guild['roles'][role]['colour'])
-        }
-        if role == '@everyone':
-            roles ['@everyone'] = ctx.guild.default_role
-            await ctx.guild.default_role.edit(**params)
-            continue
-
-        new_role = await ctx.guild.create_role(**params)
-        roles[role] = new_role
-    return roles
-
-async def create_channels(ctx: commands.Context, guild, new_roles):
-    categories = {}
-    for category in guild['categories']:
-        overwrites = {}
-
-        for overwrite in guild['categories'][category]['overwrites']:
-            overwrites.update({
-                new_roles[overwrite]: discord.PermissionOverwrite(**guild['categories'][category]['overwrites'][overwrite])
-            })
-        new_category = await ctx.guild.create_category(name=category, position=guild['categories'][category].get('position'), overwrites=overwrites)
-        categories[category] = new_category
-        
-    for channel in guild['channels']:
-        
-        params = {
-            "name": channel,
-            "position": guild['channels'][channel].get('position')
-            #category added conditionally as None is invalid for create_channel
-        }
-        if guild['channels'][channel]['overwrites']:
-            overwrites = {}
-
-            for overwrite in guild['channels'][channel]['overwrites']:
-                overwrites.update({
-                    new_roles[overwrite]: discord.PermissionOverwrite(**guild['channels'][channel]['overwrites'][overwrite])
-                })
-            params['overwrites'] = overwrites
-
-        if guild['channels'][channel]['category']:
-            if categories[guild['channels'][channel]['category']]:
-                params['category'] = categories[guild['channels'][channel]['category']] 
-
-        channel_type = guild['channels'][channel].get('type')
-
-        if channel_type == 'text':
-            new_channel = await ctx.guild.create_text_channel(**params)
-        elif channel_type == 'voice':
-            new_channel = await ctx.guild.create_voice_channel(**params)
-
-        if guild['channels'][channel]['last_message']:
-            await new_channel.send(guild['channels'][channel]['last_message'])
-
-
-class GuildManagerCog(commands.Cog):
+class ServerClone(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def clear_channels(self, ctx):
+        for ch in ctx.guild.channels:
+            try:
+                await ch.delete(reason="Server clone cleanup")
+            except:
+                pass
+
+    async def clear_roles(self, ctx):
+        for role in ctx.guild.roles:
+            if role.is_default() or role.managed:
+                continue
+            try:
+                await role.delete(reason="Server clone cleanup")
+            except:
+                pass
+
+    async def fetch_overwrites(self, element):
+        overwrites = {}
+        for target, perms in element.overwrites.items():
+            if getattr(target, "managed", False):
+                continue
+            overwrites[target.name] = dict(perms)
+        return overwrites
+
+    async def gather_channels(self, ctx):
+        data = {}
+        for ch in ctx.guild.channels:
+            if ch.type == discord.ChannelType.category:
+                continue
+            overwrites = await self.fetch_overwrites(ch)
+            data[ch.name] = {
+                "type": ch.type.name,
+                "position": ch.position,
+                "category": ch.category.name if ch.category else None,
+                "nsfw": getattr(ch, "nsfw", False),
+                "permissions_synced": ch.permissions_synced,
+                "overwrites": None if ch.permissions_synced else overwrites
+            }
+        return data
+
+    async def gather_categories(self, ctx):
+        data = {}
+        for cat in ctx.guild.categories:
+            overwrites = await self.fetch_overwrites(cat)
+            data[cat.name] = {
+                "position": cat.position,
+                "overwrites": overwrites
+            }
+        return data
+
+    async def gather_roles(self, ctx):
+        data = {}
+        for role in ctx.guild.roles:
+            if role.managed:
+                continue
+            data[role.name] = {
+                "position": role.position,
+                "colour": {
+                    "r": role.colour.r,
+                    "g": role.colour.g,
+                    "b": role.colour.b
+                },
+                "mentionable": role.mentionable,
+                "permissions": dict(role.permissions)
+            }
+        return data
+
+    async def recreate_roles(self, ctx, guild_data):
+        mapping = {}
+        for name, info in guild_data['roles'].items():
+            colour = discord.Colour.from_rgb(**info['colour'])
+            if name == '@everyone':
+                await ctx.guild.default_role.edit(
+                    mentionable=info['mentionable'],
+                    colour=colour
+                )
+                mapping[name] = ctx.guild.default_role
+                continue
+            try:
+                new_role = await ctx.guild.create_role(
+                    name=name,
+                    mentionable=info['mentionable'],
+                    colour=colour
+                )
+                mapping[name] = new_role
+            except:
+                pass
+        return mapping
+
+    async def recreate_channels(self, ctx, guild_data, roles_map):
+        categories_map = {}
+        for cat_name, cat_info in guild_data['categories'].items():
+            overwrites = {
+                roles_map.get(role): discord.PermissionOverwrite(**perms)
+                for role, perms in cat_info['overwrites'].items()
+                if role in roles_map
+            }
+            try:
+                category = await ctx.guild.create_category(
+                    name=cat_name,
+                    position=cat_info.get("position"),
+                    overwrites=overwrites
+                )
+                categories_map[cat_name] = category
+            except:
+                pass
+
+        for ch_name, ch_info in guild_data['channels'].items():
+            overwrites = None
+            if ch_info['overwrites']:
+                overwrites = {
+                    roles_map.get(role): discord.PermissionOverwrite(**perms)
+                    for role, perms in ch_info['overwrites'].items()
+                    if role in roles_map
+                }
+            params = {
+                "name": ch_name,
+                "position": ch_info.get("position"),
+                "overwrites": overwrites
+            }
+            if ch_info['category'] and ch_info['category'] in categories_map:
+                params["category"] = categories_map[ch_info['category']]
+
+            try:
+                if ch_info['type'] == 'text':
+                    await ctx.guild.create_text_channel(**params)
+                elif ch_info['type'] == 'voice':
+                    await ctx.guild.create_voice_channel(**params)
+            except:
+                pass
+
     @commands.command()
     @commands.has_permissions(administrator=True)
-    async def saveconfig(self, ctx: commands.Context, arg1):
-        server = {
-            "categories": await get_categories(ctx),
-            "channels": await get_channels(ctx),
-            "roles": await get_roles(ctx)
+    async def save(self, ctx, name: str):
+        data = {
+            "categories": await self.gather_categories(ctx),
+            "channels": await self.gather_channels(ctx),
+            "roles": await self.gather_roles(ctx)
         }
-
-        filename = f"server_configs/{arg1}.json"
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-
-        with open(filename, "w") as f:
-            f.write(json.dumps(server, indent=4))
-
-        await ctx.send('Config saved')
+        os.makedirs("backups", exist_ok=True)
+        with open(f"backups/{name}.json", "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
+        await ctx.send(f"Yedekleme başarılı: `{name}`")
 
     @commands.command()
     @commands.has_permissions(administrator=True)
-    async def loadconfig(self, ctx: commands.Context, arg1):
-        await remove_channels(ctx)
-        await remove_roles(ctx)
+    async def load(self, ctx, name: str):
+        path = f"backups/{name}.json"
+        if not os.path.isfile(path):
+            await ctx.send("Böyle bir yedek bulunamadı.")
+            return
 
+        await self.clear_channels(ctx)
+        await self.clear_roles(ctx)
 
-        guild = json.load(open(f'server_configs/{arg1}.json'))
-        new_roles = await create_roles(ctx, guild)
-        await create_channels(ctx, guild, new_roles)
-      
+        with open(path, "r", encoding="utf-8") as f:
+            guild_data = json.load(f)
 
+        roles_map = await self.recreate_roles(ctx, guild_data)
+        await self.recreate_channels(ctx, guild_data, roles_map)
+        await ctx.send(f"Yedek yüklendi: `{name}`")
 
 async def setup(bot):
-    await bot.add_cog(GuildManagerCog(bot))
+    await bot.add_cog(ServerClone(bot))
